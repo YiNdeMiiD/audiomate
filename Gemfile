c84493c3b4189aac1829f946b9c0ac7df6f04ed7
user <rentcasey@gmail.com>
2015-07-13 18:17:46 +0300

    mechanize parser + vk auth

--- Gemfile
+++ Gemfile
@@ -15,3 +15,5 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'pg'
 gem 'nokogiri'
+gem 'mechanize'
+gem 'dotenv-rails', :groups => [:development, :test]
--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,13 +1,19 @@
 class Parser
 
   require 'open-uri'
-  require 'nokogiri'
+  require 'mechanize'
+
+  def self.vk_auth
+    agent = Mechanize.new
+    login_form = agent.get('http://vkontakte.ru').forms[0]
+    login_form.email = ENV[ 'VK_LOGIN' ]
+    login_form.pass  = ENV[ 'VK_PASS' ]
+    agent.submit( agent.submit(login_form, login_form.buttons.first).forms.first )
+  end
 
   def self.vk
-    doc = Nokogiri::HTML(open("https://vk.com/audios191733152?q=moby"))
-    audio_wrapper_class = '.play_btn fl_l'
-    tracks = doc.css( audio_wrapper_class )
-    tracks
+    agent = vk_auth
+    doc = agent.get( "https://vk.com/audios80676137?q=Moby" )
   end
   
 
